77d04c45c678ef1950135c4097a211d7f12d17c8
Faris Aziz <farisaziz12@gmail.com>
2021-05-23 20:56:33 +0200

    feat(utils): export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
 export { FocusManager } from "./FocusManager";
 export { DOM } from "./DOM";
 export { PerformanceMonitor } from "./PerformanceMonitor";
+export { urlParams } from "./urlParams";
+export { logger } from "./logger";
 export * from "./functions";
