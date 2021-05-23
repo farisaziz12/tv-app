fdd4afd0bfcf10297aff2db6c16ee3af498fd904
Faris Aziz <farisaziz12@gmail.com>
2021-05-23 20:46:29 +0200

    feat(app): add sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { RoutingManager } from "./Routes";
 import { Error404 } from "./Routes";
-import Sidebar from "./components/Sidebar";
+import { Sidebar } from "./components";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles["app-container"]}>
-      <RoutingManager NotFoundComponent={Error404} />
-      <Sidebar />
+      <RoutingManager NotFoundComponent={Error404}>
+        <Sidebar />
+      </RoutingManager>
     </div>
   );
 }
