a2959bb205d0ad308af30e6677eaedb0c9d94d6d
Faris Aziz <farisaziz12@gmail.com>
2021-05-23 12:51:19 +0200

    feat(components): export

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Grid } from "./Grid";
+export { BasicCard } from "./BasicCard";
+export { GridsContainer } from "./GridsContainer";
+export { Hero } from "./Hero";
+export { Loader } from "./Loader";
