d2609391f9273a4f83dd205f353c279370e8035b
Faris Aziz <farisaziz12@gmail.com>
2021-05-23 20:50:28 +0200

    feat(components): add sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { BasicCard } from "./BasicCard";
 export { GridsContainer } from "./GridsContainer";
 export { Hero } from "./Hero";
 export { Loader } from "./Loader";
+export { Sidebar } from "./Sidebar";
