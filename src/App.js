317ad972c99eff9dd299fcf94e722effbfed7cd6
Faris Aziz <farisaziz12@gmail.com>
2021-05-23 20:55:22 +0200

    feat(app): remove sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { RoutingManager } from "./Routes";
 import { Error404 } from "./Routes";
-import { Sidebar } from "./components";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles["app-container"]}>
-      <RoutingManager NotFoundComponent={Error404}>
-        <Sidebar />
-      </RoutingManager>
+      <RoutingManager NotFoundComponent={Error404} />
     </div>
   );
 }
