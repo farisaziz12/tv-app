dd7f4d524e788ee0047d04b05b41b76127849522
Faris Aziz <farisaziz12@gmail.com>
2021-05-26 21:35:15 +0200

    feat(loader): add position props

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Loader.module.css";
 
-export function Loader() {
-  return <div className={styles.loader}></div>;
+export function Loader({ top, right }) {
+  const styleObj = top || right ? { top: `${top}vw`, right: `${right}vw` } : {};
+
+  return <div style={styleObj} className={styles.loader}></div>;
 }
