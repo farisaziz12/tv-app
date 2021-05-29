6340e8d76499cac9e5c232fdbe0ad9955e37723b
Faris Aziz <farisaziz12@gmail.com>
2021-05-29 21:58:17 +0200

    feat(video-player): add progress and controls handler

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import videojs from "video.js";
-import { Controls, isPaused$, progress$ } from "./Controls";
-import { FocusManager, keyHandler, logger, formatTime } from "../../utils";
+import { Controls, isPaused$, progress$, playerControls$ } from "./Controls";
+import { FocusManager, keyHandler, logger } from "../../utils";
 import "video.js/dist/video-js.css";
 import styles from "./VideoPlayer.module.css";
+import { pathOr } from "ramda";
 
 export class VideoPlayer extends Component {
   constructor() {
     super();
     this.videoJsOptions = {
-      autoplay: true,
+      autoplay: "play",
       sources: [
         {
-          src: "https://s3.amazonaws.com/senkorasic.com/test-media/video/caminandes-llamigos/caminandes_llamigos_1080p.mp4",
+          src: "https://s3.amazonaws.com/senkorasic.com/test-media/video/caminandes-llamigos/caminandes_llamigos_720p.mp4",
           type: "video/mp4",
         },
       ],
     };
 
     this.keyConfig = {
-      Enter: this.handlePlayPause,
       Backspace: this.handleExit,
     };
   }
@@ -28,28 +28,58 @@ export class VideoPlayer extends Component {
     this.player = videojs(this.videoNode, this.videoJsOptions, function onPlayerReady() {
       logger("onPlayerReady", this).log();
     });
+    this.player.on("ended", () => {
+      logger("video ended").log();
+      this.handleExit();
+    });
+
     this.handlePlayer = this.props.handlePlayer;
 
-    this.progressInterval = setInterval(() => {
-      if (!this.player.paused()) {
-        const secondsRemaining = this.player.remainingTime();
-        const currentTime = this.player.currentTime();
-        const duration = this.player.duration();
-        progress$.next({
-          duration,
-          currentTime,
-          secondsRemaining,
-          displayTime: formatTime(secondsRemaining),
-        });
-      }
-    }, 1000);
+    this.progressInterval = setInterval(this.handleProgressInterval, 1000);
+
+    this.playerControls$ = playerControls$.subscribe(this.handlePlayerControls);
   }
+
+  handlePlayerControls = (component) => {
+    switch (component) {
+      case "play-pause-button":
+        this.handlePlayPause();
+        break;
+      case "forward-button":
+        this.player.currentTime(this.player.currentTime() + 15);
+        break;
+      case "back-button":
+        this.player.currentTime(this.player.currentTime() - 15);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  handleProgressInterval = () => {
+    if (!this.player.paused()) {
+      const displayTime = pathOr(
+        "00:00",
+        ["controlBar", "remainingTimeDisplay", "formattedTime_"],
+        this.player
+      );
+      const currentTime = this.player.currentTime() ?? 0;
+      const duration = this.player.duration() ?? 0;
+      progress$.next({
+        duration,
+        currentTime,
+        displayTime,
+      });
+    }
+  };
 
   componentWillUnmount() {
     if (this.player) {
       this.player.dispose();
     }
     clearInterval(this.progressInterval);
+    this.playerControls$.unsubscribe();
   }
 
   handleExit = () => {
