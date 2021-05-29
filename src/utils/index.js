3d05e33dc0e265770e4c27beeed22457690f7fc1
Faris Aziz <farisaziz12@gmail.com>
2021-05-29 23:39:43 +0200

    bootstrap network checker func

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,4 @@ export { PerformanceMonitor } from "./PerformanceMonitor";
 export { urlParams } from "./urlParams";
 export { logger } from "./logger";
 export * from "./functions";
+export * from "./bootstrapNetworkChecker";
