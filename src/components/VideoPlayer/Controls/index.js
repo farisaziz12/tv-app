627bd1d82a46f82593999f1fbf94a042c3f5c4b4
Faris Aziz <farisaziz12@gmail.com>
2021-05-29 21:57:47 +0200

    feat(controls): add controls sleep and focus

--- src/components/VideoPlayer/Controls/index.js
+++ src/components/VideoPlayer/Controls/index.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { pathOr } from "ramda";
 import { BehaviorSubject, Subject } from "rxjs";
+import { throttleTime } from "rxjs/operators";
+import { ControlsButton } from "./ControlsButton";
 import { FocusManager, logger } from "../../../utils";
 import styles from "./Controls.module.css";
 
-export const isPaused$ = new BehaviorSubject(false);
 export const progress$ = new Subject();
+export const playerControls$ = new Subject();
+export const isPaused$ = new BehaviorSubject(false);
 const wakeControls$ = new BehaviorSubject(true);
+const wakeTime = 6000;
 
 export function Controls() {
   const [isPaused, setIsPaused] = useState(false);
@@ -13,21 +18,22 @@ export function Controls() {
   const [progress, setProgress] = useState({
     duration: 0,
     currentTime: 0,
-    secondsRemaining: 0,
     displayTime: "00:00",
   });
 
   useEffect(() => {
     const focusManager = new FocusManager();
-    focusManager.getComponent("play-pause").focus();
+    focusManager.getComponent("play-pause-button").focus();
 
     const isPausedSubscription = isPaused$.subscribe(setIsPaused);
-    const wakeControlsSubscription = wakeControls$.subscribe(() => {
-      setIsAwake(true);
-      setTimeout(() => {
-        setIsAwake(false);
-      }, 7000);
-    });
+    const wakeControlsSubscription = wakeControls$
+      .pipe(throttleTime(wakeTime))
+      .subscribe(() => {
+        setIsAwake(true);
+        setTimeout(() => {
+          setIsAwake(false);
+        }, wakeTime);
+      });
     const progressSubscription = progress$.subscribe((remainingTime) => {
       logger(remainingTime).log();
       setProgress(remainingTime);
@@ -40,6 +46,18 @@ export function Controls() {
     };
   }, []);
 
+  const handleControls = (event) => {
+    wakeControls$.next();
+    const component = pathOr("", ["target", "dataset", "component"], event);
+
+    if (event.key === "Enter") {
+      playerControls$.next(component);
+    } else {
+      const focusManager = new FocusManager(event);
+      focusManager.playerControlFocus();
+    }
+  };
+
   const { currentTime, duration } = progress;
   const progressWidth = currentTime / duration ? (currentTime / duration) * 100 : 0;
 
@@ -47,20 +65,29 @@ export function Controls() {
     <div
       style={{ opacity: isAwake ? "1" : "0" }}
       className={styles["controls-dashboard"]}
-      onKeyDown={() => wakeControls$.next()}
     >
       <div className={styles["progress-bar"]}>
         <div style={{ width: `${progressWidth}%` }} className={styles.progress}></div>
         <div className={styles["remaining-time"]}>{progress.displayTime}</div>
       </div>
-      <div className={styles["controls-container"]}>
-        <div className={styles["focus-container"]}>
-          <div
-            tabIndex="-1"
-            data-component="play-pause"
-            className={isPaused ? styles.play : styles.pause}
-          ></div>
-        </div>
+      <div data-component="controls-container" className={styles["controls-container"]}>
+        <ControlsButton
+          className={styles["seek-button"]}
+          component="back-button"
+          handler={handleControls}
+          text="15-"
+        />
+        <ControlsButton
+          className={isPaused ? styles.play : styles.pause}
+          component="play-pause-button"
+          handler={handleControls}
+        />
+        <ControlsButton
+          className={styles["seek-button"]}
+          component="forward-button"
+          handler={handleControls}
+          text="15+"
+        />
       </div>
     </div>
   );
