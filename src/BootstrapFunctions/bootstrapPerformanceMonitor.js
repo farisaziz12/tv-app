ff2b94d7b82451c793eef0cc8dfa1656f2ed6145
Faris Aziz <farisaziz12@gmail.com>
2021-05-30 15:37:39 +0200

    feat(bootstrap-performance-monitor): create

---
+++ src/BootstrapFunctions/bootstrapPerformanceMonitor.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { StatsGraph } from "@helpscout/stats";
+import { urlParams } from "../utils/urlParams";
+
+function PerformanceMonitor({ children }) {
+  const isDebugMode = urlParams("debug");
+
+  useEffect(() => {
+    if (isDebugMode) {
+      const graphs = Array.from(document.querySelectorAll("canvas"));
+      graphs.forEach((graph) => {
+        graph.style.width = "9vw";
+        graph.style.height = "7vw";
+      });
+    }
+  }, [isDebugMode]);
+
+  const renderPerformanceGraphs = () => {
+    if (isDebugMode) {
+      return <StatsGraph />;
+    }
+  };
+
+  return (
+    <div>
+      {renderPerformanceGraphs()}
+      {children}
+    </div>
+  );
+}
+
+export const bootstrapPerformanceMonitor = (app) => {
+  return <PerformanceMonitor>{app}</PerformanceMonitor>;
+};
