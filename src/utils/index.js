ab788a9f95d5f11f7e89300e6c09c4e09987ba11
Faris Aziz <farisaziz12@gmail.com>
2021-05-30 15:39:43 +0200

    refactor(): move to bootstrap functions dir

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,5 @@
 export { FocusManager } from "./FocusManager";
 export { DOM } from "./DOM";
-export { PerformanceMonitor } from "./PerformanceMonitor";
 export { urlParams } from "./urlParams";
 export { logger } from "./logger";
 export * from "./functions";
-export * from "./bootstrapNetworkChecker";
