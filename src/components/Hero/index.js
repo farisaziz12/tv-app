8206ac00751ec859190387da732d0df598dcc091
Faris Aziz <farisaziz12@gmail.com>
2021-05-30 18:03:38 +0200

    feat(hero): add image fallback, genres and release data

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,3 +1,4 @@
+import { propOr } from "ramda";
 import React, { useState, useEffect } from "react";
 import { Subject } from "rxjs";
 import { DOM } from "../../utils";
@@ -28,14 +29,42 @@ export function Hero() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [card]);
 
+  const renderImage = () => {
+    const { backdropUrl = "" } = card;
+    if (backdropUrl) {
+      return <img src={backdropUrl} alt="" />;
+    } else {
+      return <div className={styles["img-fallback"]}>TV App</div>;
+    }
+  };
+
+  const renderGenres = () => {
+    const genres = propOr([], ["genres"], card);
+
+    if (genres[0]) {
+      return (
+        <div className={styles["genres-container"]}>
+          {genres.map((genre, index) => (
+            <div key={index}>
+              <span className={styles.dot}></span>
+              <div className={styles.genre}>{genre}</div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
     <div className={styles["hero-cover"]}>
       <div data-component="hero" className={styles["hero"]}>
         <div className={styles["metadata-container"]}>
           <div className={styles.title}>{card?.title}</div>
+          <div className={styles["release-date"]}>{card?.releaseDate}</div>
+          {renderGenres()}
           <div className={styles.description}>{card?.description}</div>
         </div>
-        <img src={card?.backdropUrl} alt="" />
+        {renderImage()}
       </div>
     </div>
   );
