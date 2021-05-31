13915ff67679387df12f4ba07b50e605cd78e8fe
Faris Aziz <farisaziz12@gmail.com>
2021-05-31 17:36:45 +0200

    feat(sidebar): listen to history change for focus

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SidebarItem } from "./SidebarItem";
+import { FocusManager } from "../../utils";
+import { useHistory } from "react-router";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      const focusManager = new FocusManager();
+      focusManager.handleFocusLastCard();
+    });
+
+    return () => {
+      unlisten();
+    };
+  }, [history]);
+
   return (
     <div className={styles.sidebar}>
       <div data-component="sidebar-items" className={styles["sidebar-items-container"]}>
         <SidebarItem href="/home">Home</SidebarItem>
-        <SidebarItem href="/homes">fail</SidebarItem>
-        <SidebarItem href="/home">Home</SidebarItem>
-        <SidebarItem href="/home">Home</SidebarItem>
-        <SidebarItem href="/home">Home</SidebarItem>
-        <SidebarItem href="/home">Home</SidebarItem>
+        <SidebarItem href="/genre-page/horror">Horror</SidebarItem>
+        <SidebarItem href="/genre-page/action">Action</SidebarItem>
+        <SidebarItem href="/genre-page/comedy">Comedy</SidebarItem>
+        <SidebarItem href="/genre-page/family">Family</SidebarItem>
+        <SidebarItem href="/homes">Fail</SidebarItem>
       </div>
     </div>
   );
