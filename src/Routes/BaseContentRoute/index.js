89fa20389e7765b1f93d44009ffbfb5043bc9c4e
Faris Aziz <farisaziz12@gmail.com>
2021-05-31 17:37:28 +0200

    feat(base-content-route): add get genre

--- src/Routes/BaseContentRoute/index.js
+++ src/Routes/BaseContentRoute/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { getGenreId } from "../../API";
 import { VideoPlayer } from "../../components";
+import { urlParams } from "../../utils";
 
 export default class BaseContentRoute extends Component {
   constructor() {
@@ -12,6 +14,11 @@ export default class BaseContentRoute extends Component {
     this.renderVideoPlayer.bind(this);
     this.handlePlayer.bind(this);
   }
+
+  getGenre = async () => {
+    const genreId = await getGenreId(urlParams("genre"));
+    return genreId;
+  };
 
   handlePlayer = () => {
     this.setState((prevState) => ({ showPlayer: !prevState.showPlayer }));
