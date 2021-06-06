a9300d11ef1968bca84184b5ce76195da4213e3e
Faris Aziz <farisaziz12@gmail.com>
2021-06-06 22:22:22 +0200

    refactor(resolve-component): use types enum

--- src/utils/resolveComponent.js
+++ src/utils/resolveComponent.js
@@ -1,10 +1,11 @@
-import { DisplayCard, BasicCard } from "../components";
+import { BasicCard, DisplayCard } from "../components";
+import { TYPES } from "../Enums";
 
 export const resolveComponent = (component, props) => {
   switch (component) {
-    case "basic":
+    case TYPES.BASIC_COMPONENT:
       return <BasicCard key={props.id} {...props} />;
-    case "display":
+    case TYPES.DISPLAY_COMPONENT:
       return <DisplayCard key={props.id} {...props} />;
 
     default:
