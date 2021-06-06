d523167437220b5544785dda0a38568192f0c527
Faris Aziz <farisaziz12@gmail.com>
2021-06-06 22:21:57 +0200

    refactor(logger): use types enum

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,8 @@
+import { TYPES } from "../Enums";
 import { urlParams } from "./urlParams";
 
 export const logger = (...args) => {
-  const isDebugMode = urlParams("debug");
+  const isDebugMode = urlParams(TYPES.DEBUG);
   const log = () => isDebugMode && console.log(...args);
   const warn = () => isDebugMode && console.warn(...args);
   const error = () => isDebugMode && console.error(...args);
